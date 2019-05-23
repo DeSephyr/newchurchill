fd8509ac19b28eed588679cf813ac755c93668cb
DeSephyr <lehich800@gmail.com>
2019-05-23 10:56:32 +0200

    deviceCard, imgDevice etc

--- js/main.js
+++ js/main.js
@@ -1,71 +1,71 @@
-$(document).ready(function() {
-	
+$(document).ready(function () {
+
 	$(function () {
 		$("a[data-target^='anchor']").click(function () {
 			var _href = $(this).attr("href");
-			$("html, body").animate({ scrollTop: $(_href).offset().top -50 + "px"}, 700);
+			$("html, body").animate({
+				scrollTop: $(_href).offset().top - 50 + "px"
+			}, 700);
 			return false;
 		});
 	});
-	$( '.shop-wrap_device').hover(
-		function(){
-	   		// $( '.shop-feature-wrap_device').slideToggle().css("display", "flex");
-	   		$( '.shop-feature-wrap_device').css("display", "flex");
-	   		$('.shop').css("margin-bottom", "0");
-	    },
-	    function(){
-	   		// $( '.shop-feature-wrap_device').slideToggle();
-	    	$( '.shop-feature-wrap_device').css("display", "none");
-	   		$('.shop').css("margin-bottom", "135px");
-	});
-	$( '.shop-wrap_liquids').hover(
-		function(){
-	   		// $( '.shop-feature-wrap_liquids').slideToggle().css("display", "flex");
-	    	$( '.shop-feature-wrap_liquids').css("display", "flex");
-	   		$('.shop').css("margin-bottom", "0");
-	    },
-	    function(){
-	   		// $( '.shop-feature-wrap_liquids').slideToggle();
-	    	$( '.shop-feature-wrap_liquids').css("display", "none");
-	   		$('.shop').css("margin-bottom", "135px");
-	});
+	$('.shop-wrap_device').hover(
+		function () {
+			// $( '.shop-feature-wrap_device').slideToggle().css("display", "flex");
+			$('.shop-feature-wrap_device').css("display", "flex");
+			$('.shop').css("margin-bottom", "0");
+		},
+		function () {
+			// $( '.shop-feature-wrap_device').slideToggle();
+			$('.shop-feature-wrap_device').css("display", "none");
+			$('.shop').css("margin-bottom", "135px");
+		});
+	$('.shop-wrap_liquids').hover(
+		function () {
+			// $( '.shop-feature-wrap_liquids').slideToggle().css("display", "flex");
+			$('.shop-feature-wrap_liquids').css("display", "flex");
+			$('.shop').css("margin-bottom", "0");
+		},
+		function () {
+			// $( '.shop-feature-wrap_liquids').slideToggle();
+			$('.shop-feature-wrap_liquids').css("display", "none");
+			$('.shop').css("margin-bottom", "135px");
+		});
 	$('.devices-slider').slick({
 		slidesToShow: 1,
 		centerMode: true,
 		variableWidth: true,
-	  	nextArrow: $('.devices-slider-arrow-next'), 
-	  	prevArrow: $('.devices-slider-arrow-prev'),
-		responsive: [
-		{
-		  breakpoint: 480,
-		  settings: {
-		    centerMode: false,
-			variableWidth: false,
-			dots: true,
-		    slidesToShow: 1
-		  }
-		}
-		]
+		nextArrow: $('.devices-slider-arrow-next'),
+		prevArrow: $('.devices-slider-arrow-prev'),
+		responsive: [{
+			breakpoint: 480,
+			settings: {
+				centerMode: false,
+				variableWidth: false,
+				dots: true,
+				slidesToShow: 1
+			}
+		}]
 	});
 	$('.comments-slider').slick({
 		dots: true,
-	  	nextArrow: $('.comments-slider-arrow-next'), 
-	  	prevArrow: $('.comments-slider-arrow-prev')
+		nextArrow: $('.comments-slider-arrow-next'),
+		prevArrow: $('.comments-slider-arrow-prev')
 	});
 	$('.open-popup-link').magnificPopup({
-		type:'inline',
+		type: 'inline',
 		midClick: true
 	});
 	$('button.open-popup-link').magnificPopup({
-	  items: {
-	      src: '#request-popup',
-	      type: 'inline'
-	  }
+		items: {
+			src: '#request-popup',
+			type: 'inline'
+		}
 	});
 	$('.navbar-basket').click(function () {
 		$('#hmt').trigger('click');
 	});
-	
+
 	// if($('#dont-smoke:radio:checked')) {
 	// 	$('#btn_result').click(function () {
 	// 		$('.quiz-answer#quiz-dont-smoke').show();
@@ -81,15 +81,21 @@ $(document).ready(function() {
 	// 		$('.quiz-answer#quiz-medium').show();
 	// 	});
 	// }
-	$('input[name=quiz-radio]').change(function() {
-	const val = $(this).val();
-    if($(this).val(val)) {
-		$('#btn_result').click(function () {
-			$('#quiz-' + val).css('display', 'flex');
-		});
-	}
-
+	$('input[name=quiz-radio]').change(function () {
+		const val = $(this).val();
+		if ($(this).val(val)) {
+			$('#btn_result').click(function () {
+				$('#quiz-' + val).css('display', 'flex');
+			});
+			$('.btn_answer').click(function () {
+				$('#quiz-' + val).css('display', 'none');
+			});
+		}
 	});
+ 
+	
+
+
 	// if($('#hard:checked')) {
 	// 	$('#btn_result').click(function () {
 	// 		$('#quiz-hard').show();
@@ -100,7 +106,7 @@ $(document).ready(function() {
 	// 		$('#quiz-dont-know').show();
 	// 	});
 	// }
-	
+
 	// $('a#close').click(function () {
 	// 	$('#mfp-bg').hide();
 	// });
