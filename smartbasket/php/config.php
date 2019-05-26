b6a1c46babe11e04f9f20a5c287ebfa50a2ce18a
DeSephyr <lehich800@gmail.com>
2019-05-26 15:28:33 +0200

    last

--- smartbasket/php/config.php
+++ smartbasket/php/config.php
@@ -1,8 +1,8 @@
 <?php
     require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/phpmailer.php');
    
-    const SENDER = 'sender@yandex.ru';
-    const CATCHER = 'catcher@list.ru';
+    const SENDER = 'lehich800@gmail.com';
+    const CATCHER = 'alexfaustovdesign@gmail.com';
     const SUBJECT = 'Заявка с сайта';
     const CHARSET = 'UTF-8';
     
